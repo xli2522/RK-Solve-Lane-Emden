542a9a7451f2f1719128a9365702670de8966c91
sofiapasquini <65981514+sofiapasquini@users.noreply.github.com>
2021-02-17 16:24:04 -0500

    Update rk_solve.py
    added the analytical solutions for n=0,1,5; also added titles on the plot

--- rk_solve.py
+++ rk_solve.py
@@ -1,6 +1,7 @@
 # Solve Lane-Emden with RK2
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 '''ODE Model:
     dudxi = v
@@ -42,7 +43,32 @@ for n in [1.5, 3, 3.25]:
         vplot.append(v)
         xiplot.append(xi)
         count += 1
-    ax.plot(xiplot, uplot)      # plot xi vs. u under each n
+    ax.plot(xiplot, uplot, label="n="+str(n))      # plot xi vs. u under each n
+    
+#define functions for the analytical solutions for n=0,1,5
+def theta0(zeta):
+	theta=1-(zeta**2/6)
+	return theta
+def theta1(zeta):
+	theta=math.sin(zeta)/zeta
+	return theta
+def theta5(zeta):
+	theta=(1+(zeta**2/3))**(-0.5)
+	return theta
+
+#generate the analytical solutions to be plotted
+thetas_0=[]
+thetas_1=[]
+thetas_5=[]
+for i in range(len(xiplot)):
+	thetas_0.append(theta0(xiplot[i]))
+	thetas_1.append(theta1(xiplot[i]))
+	thetas_5.append(theta5(xiplot[i]))
+
+#plot the analytic solutions over top of the numeric solutions
+ax.plot(xiplot,thetas_0, label="n=0 (analytic)")
+ax.plot(xiplot,thetas_1, label="n=1 (analytic)")
+ax.plot(xiplot,thetas_5, label="n=5 (analytic)")
 
 # Calculation of q2 (a)
 xiRe = xiplot.pop()      # xi at the surface        7.979999999999874
@@ -52,13 +78,10 @@ Msun = 1.99*10**33      # g
 rhoc = - (Msun * xiRe)/(4*np.pi*Rsun**3*vRe)
 print(rhoc)         # 125.09001252015766
 
-'''# analytical
-for n in [0, 1, 5]:
-    uAnalytical = []
-    xiAnalytical = []
-    xiAnalytical.append()'''
-
-ax.set_xlim([0, 10])        # set x axis limit
+ax.set_xlim([0, max(xiplot)])        # set x axis limit
 ax.set_ylim([0, 1.0])       # set y axis limit
-ax.legend(['n = 1.5','n = 3','n = 3.5'])
+ax.set_xlabel('\u03B6')     # set axis titles
+ax.set_ylabel('\u03B8')
+ax.set_title("Numeric and Analytic Solutions to the Lane-Emden Equation")       # set plot title
+ax.legend()
 plt.show()      # show plot
